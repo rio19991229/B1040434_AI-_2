f9545d64d4388ba81fa9dd78f09f05b98d0cf24d
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-09-26 12:07:26 +0800

    AI2019/9/26

---
+++ 2D/Assets/script/Fox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+	void Start ()
+    {
+     Debug.Log("海狗騎太慢");
+     Debug.Log("啊!!!被撞了");
+     Debug.Log("媽媽說要照速限");
+     Debug.Log("主要是沒保持安全車距");
+     Debug.Log("Yo");
+     Debug.LogWarning("GG");
+     Debug.LogError("Fall");
+
+    }
+
+}
