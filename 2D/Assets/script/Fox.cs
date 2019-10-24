560c61aa68f2f78f0ccf79c182c8496b7b14035f
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-10-24 12:03:45 +0800

    20191024

--- 2D/Assets/script/Fox.cs
+++ 2D/Assets/script/Fox.cs
@@ -1,11 +1,34 @@
-﻿using UnityEngine;
+﻿using UnityEngine;                  // 引用 Unity API - API 倉庫 功能、工具
 
-public class Fox : MonoBehaviour
+public class Fox : MonoBehaviour    // 類別 類別名稱
 {
-	void Start ()
+    // 成員：欄位、屬性、方法、事件
+    // 修飾詞 類型 名稱 指定 值；
+    public int speed = 50;                  // 整數
+    public float jump = 2.5f;               // 浮點數
+    public string foxName = "狐狸";         // 字串
+    public bool pass = false;               // 布林值 - true/false
+
+    private Rigidbody2D r2d;
+    private Transform tra;
+
+    // 事件：在特定時間點會以指定頻率執行的方法
+    // 開始事件：遊戲開始時執行一次
+    private void Start()
     {
-     Debug.Log("Hello");
-
+        // 泛型 <T>
+        r2d = GetComponent<Rigidbody2D>();
+        tra = GetComponent<Transform>();
     }
-
+    //Debug.Log(Input.GetAxis("Horizontal"));
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) tra.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) tra.eulerAngles = new Vector3(0, 180, 0);
+    }
+    private void FixedUpdate()
+    {
+        r2d.AddForce(new Vector2(speed * (Input.GetAxis("Horizontal")), 0));
+        
+    }
 }
